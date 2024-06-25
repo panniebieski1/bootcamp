35ea0fdd3d153f9736ec77359475ee391acfeea0
panniebieski1 <adrian.galeski@gmail.com>
2024-06-25 22:02:59 +0200

    licznik

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -4,5 +4,21 @@
     <br />
     <br />
     <p>hej blockchain</p>
+    {{ licznik }}
+    <button @click="podbij">Podbij</button>
   </main>
 </template>
+<script>
+export default{
+  data() {
+    return{
+      licznik: 0 
+    }
+  },
+  methods: {
+    podbij() {
+      this.licznik++
+    }
+  }
+}
+</script>
